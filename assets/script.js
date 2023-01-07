6e94b19c2ef5b2d3c9e107cc19ef9428b6dee1f5
Marcelo Santana <marcelo.ssantos127@gmail.com>
2023-01-07 13:33:38 -0300

    Slide buttons

--- assets/script.js
+++ assets/script.js
@@ -9,12 +9,58 @@ function handler() {
   console.log({ tamanhoSlide, quantidadeSlides, tamanhoContainer });
 
   let first = tamanhoSlide - (parseInt(tamanhoSlide / 260)) * 275;
-  console.log(first);
+  console.log();
+
+
+  let buttonNext = document.querySelector("#next");
+  buttonNext.addEventListener('click', function () {
+
+    let style = document.querySelector(".projects__menu").getAttribute('style');
+
+    if (style == null) {
 
 
 
 
-  // document.querySelector(".projects__menu").setAttribute('style', `transform: translate3d(-${260 + n}px, 0px, 0px)`);
+      document.querySelector(".projects__menu").setAttribute('style', `transform: translate3d(-${274 - first}px, 0px, 0px)`);
+    } else {
+      const translate3d = style.match(/translate3d\(([^)]+)\)/)[1];
+      let [a, b, c] = translate3d.split(', ');
+      a = a.replace(/px$/, '');
 
+      let checkFinal = tamanhoContainer - tamanhoSlide;
+      let abs = Math.abs(a);
+      console.log(abs);
+
+      if ((abs + 275) < checkFinal) {
+        document.querySelector(".projects__menu").setAttribute('style', `transform: translate3d(${a - 275}px, 0px, 0px)`);
+      }
+    }
+  });
+
+  let buttonPrevious = document.querySelector("#previous");
+  buttonPrevious.addEventListener('click', function () {
+
+    let style = document.querySelector(".projects__menu").getAttribute('style');
+
+    if (style == null) {
+
+    } else {
+      const translate3d = style.match(/translate3d\(([^)]+)\)/)[1];
+      let [a, b, c] = translate3d.split(', ');
+      a = parseInt(a.replace(/px$/, ''));
+
+      // let checkFinal = tamanhoContainer - tamanhoSlide;
+      let abs = Math.abs(a);
+      console.log("AAA " + abs);
+      // console.log(abs);
+      if (abs < 275) {
+        document.querySelector(".projects__menu").setAttribute('style', `transform: translate3d(${0}px, 0px, 0px)`);
+      } else {
+        document.querySelector(".projects__menu").setAttribute('style', `transform: translate3d(${a + 275}px, 0px, 0px)`);
+      }
+
+    }
+  });
 
 }
